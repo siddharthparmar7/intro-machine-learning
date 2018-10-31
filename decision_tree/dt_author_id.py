a5ced373ccd144f44ca9bbddb2e8f72c6758b0a7
Sid Parmar <Sid@Sids-MacBook-Pro.local>
2018-10-30 19:06:06 -0700

    Decision Tree exercises solved

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -7,25 +7,28 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
 
 
 #########################################################
 ### your code goes here ###
-
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+print 'number of features in our data', len(features_train[0])
+clf.fit(features_train, labels_train)
+prediction = clf.predict(features_test)
+print 'Prediction: ', prediction
+acc = clf.score(features_test, labels_test)
+print 'Accuracy: ', acc
 
 #########################################################
-
-
