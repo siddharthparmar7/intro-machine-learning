2ab72552d7b271cf64bed09fb9862dfdedb9e204
Sid Parmar <Sid@Sids-MacBook-Pro.local>
2019-01-20 15:19:15 -0800

    text learning lesson done

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,23 +41,31 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # temp_counter += 1
+        # if temp_counter < 200:
+        path = os.path.join('..', path[:-1])
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        ### use parseOutText to extract the text from the opened email
+        stemed_email = parseOutText(email)
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        words_to_remove = ["sara", "shackleton", "chris", "germani"]
+        for word in words_to_remove:
+            stemed_email.replace(word, '')
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        ### append the text to word_data
+        word_data.append(stemed_email)
 
-            ### append the text to word_data
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        if name == 'sara':
+            from_data.append(0)
+        else: 
+            from_data.append(1)
+        email.close()
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
+#### quiz 19 answer
+print word_data[152]
 
 print "emails processed"
 from_sara.close()
@@ -71,5 +79,9 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(word_data)
+print((vectorizer.get_feature_names()[34597]))
 
 
